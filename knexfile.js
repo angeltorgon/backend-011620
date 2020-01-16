8f3bf3aa0b5ac054905bbe09a47570d262a15357
angel-torres <alas4224@gmail.com>
2020-01-16 12:42:07 -0600

    created migration file

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,14 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/data.db3'
+    }, 
+    useNullAsDefault: true,
+    seeds: {
+      directory: './data/seeds'
+    },
+    migrations: {
+      directory: './data/migrations'
     }
   },
 
